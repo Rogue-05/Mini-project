825193987a5787587536dd9bb538878043658e75
Rogue-05 <152081196+Rogue-05@users.noreply.github.com>
2023-11-27 09:43:45 +0530

    Update project.py

--- project.py
+++ project.py
@@ -11,5 +11,11 @@ cxn=mysql.connect(user='root',password='Rogue@05',host='localhost')
 if cnx.isconnected():
   print('connected')
   cur=cnx.cursor()
-  cur.execute('create database if not exists')
+  cur.execute('create database if not exists project;')
+  cur.execute('use project;')
+  q1=''' create table if not exists details(
+         slno int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+         NAME varchar(50) NOT NULL,
+         AGE int NOT NULL,
+         
 
