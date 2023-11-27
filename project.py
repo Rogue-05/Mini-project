8327cc658f7137e3fcfd6328c3f6f13ec8c6e7f4
Rogue-05 <152081196+Rogue-05@users.noreply.github.com>
2023-11-27 09:39:45 +0530

    Update project.py

--- project.py
+++ project.py
@@ -1,3 +1,15 @@
 from tkinter import *
 import mysql.connector as mysql
-from tkinter import messagebox
+from tkinter import messagebox 
+import customtkinter as ctk
+
+root = Tk() 
+root.title('PES INTERNATIONAL BANK')
+root.geometry('1000x1000')
+cxn=mysql.connect(user='root',password='Rogue@05',host='localhost')
+
+if cnx.isconnected():
+  print('connected')
+  cur=cnx.cursor()
+  cur.execute('create database if not exists')
+
